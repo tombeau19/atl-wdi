7528bce853733d8b9fd42656fd939008254b40af
tombeau19 <thomas.beauregard@gmail.com>
2017-09-19 13:38:40 -0400

    completed GET routes

--- homework/pizza-express/app.js
+++ homework/pizza-express/app.js
@@ -5,7 +5,21 @@ const hbs = require('hbs');
 app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
+app.get('/', (req, res) => {
+    res.send('Welcome to Pizza Express');
+});
+
+app.get('/topping/:type', (req, res) => {
+    res.send(`${req.params.type} pizza! Good choice.`);
+})
+
+app.get('/order/:amount/:size', (req, res) => {
+    res.send(`Your order for ${req.params.amount} ${req.params.size} pizza(s) will be ready in 1 minute!`)
+})
+
 const PORT = 3002;
 app.listen(PORT, () => {
+    console.log('========================');
     console.log("listening on port", PORT);
+    console.log('========================');
 });
