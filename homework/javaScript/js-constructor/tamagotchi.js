fe0d161ead12addbef32e66a29715ffc12abafce
tombeau19 <thomas.beauregard@gmail.com>
2017-09-25 08:53:15 -0400

    tamagotchi

--- homework/javaScript/js-constructor/tamagotchi.js
+++ homework/javaScript/js-constructor/tamagotchi.js
@@ -2,18 +2,23 @@ console.log('tamagotchi file is loaded');
 
 //your class declaration here
 class Tamagotchi {
-    constructor(name, creatureType){
+    constructor(name, creatureType) {
         this.foodInTummy = 10;
         this.restedness = 10;
         this.health = 10;
         this.name = name;
         this.creatureType = creatureType;
     }
-    cry(){
+    cry() {
         this.foodInTummy--;
         console.log(`${this.name} has ${this.foodInTummy} in it's tummy`)
         console.log('WAHH!!!')
     }
+    puke() {
+        this.foodInTummy--;
+        console.log(`${this.foodInTummy} WAHH!!!`)
+    }
+
 }
 
 //create new Tamagotchis
