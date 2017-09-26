35acba2cb51c581d7d6f9edf8c54166b3baae907
tombeau19 <thomas.beauregard@gmail.com>
2017-09-26 18:37:18 -0400

    successfully seeded Donut Data

--- homework/mongo/donut-shop/donut_mongoose/controllers/donuts.js
+++ homework/mongo/donut-shop/donut_mongoose/controllers/donuts.js
@@ -3,6 +3,7 @@
 //======================
 const express = require('express');
 const router = express.Router();
+const Schema = require('../models/donuts.js')
 
 
 //======================
--- homework/mongo/donut-shop/donut_mongoose/models/donuts.js
+++ homework/mongo/donut-shop/donut_mongoose/models/donuts.js
@@ -1,7 +1,23 @@
 //requirements: require mongoose
-
+var mongoose = require("mongoose");
+mongoose.Promise = global.Promise
 
 //create your donut schema:
+const Schema = mongoose.Schema
+
+var DonutSchema = new Schema({
+    //write your schema fields here
+    name: String,
+    description: String,
+    img: String,
+    price: Number,
+    qty: Number
+    });
 
 
 //export your donut with module.exports()
+const DonutModel = mongoose.model('Donuts', DonutSchema)
+
+module.exports = {
+    DonutModel: DonutModel
+}
