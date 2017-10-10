951cc7f2e6de2a63684204b7dc6ec0e499dbb727
tombeau19 <thomas.beauregard@gmail.com>
2017-10-10 19:55:26 -0400

    finished hw

--- homework/react/bootflix/src/App.js
+++ homework/react/bootflix/src/App.js
@@ -2,23 +2,59 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
 import Movie from './components/Movie';
-import example from './omdbExample.json'
+
+import axios from 'axios'
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      movie: example
-    }
+
+  state = {
+    title: '',
+    id: '',
+    year: '',
+    director: '',
+    genre: '',
+    plot: ''
   }
 
   //Update these methods to make axios calls to OMDB and update this.state.movie with the response from the server
-  _searchByTitle = () => {
-    console.log("Search by Title");
+  searchByTitle = (event) => {
+    event.preventDefault()
+    const title = event.target.title.value
+
+    axios.get(`http://www.omdbapi.com/?apikey=d31f1a94&t=${title}`)
+    
+      .then((response) => {
+        this.setState({ 
+          title: response.data.Title,
+          year: response.data.Year,
+          director: response.data.Director,
+          genre: response.data.Genre,
+          plot: response.data.Plot
+         })
+      })
+      .catch((error) => {
+        console.error('Error: ', error)
+      })
   }
 
-  _searchById = () => {
-    console.log("Search by ID");
+ searchById = (event) => {
+    event.preventDefault()
+    const id = event.target.id.value
+
+    axios.get(`http://www.omdbapi.com/?apikey=d31f1a94&i=${id}`)
+    .then((response) => {
+      this.setState({ 
+        title: response.data.Title,
+        year: response.data.Year,
+        director: response.data.Director,
+        genre: response.data.Genre,
+        plot: response.data.Plot
+       })
+
+    })
+    .catch((error) => {
+      console.error('Error: ', error)
+    })
   }
 
   //Pass _searchByTitle, _searchById, and this.state.movie to it's appropriate child components.
@@ -26,8 +62,8 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        <Search />
-        <Movie />
+        <Search searchByTitle={this.searchByTitle} searchById={this.searchById} />
+        <Movie title={this.state.title} year={this.state.year} director={this.state.director} genre={this.state.genre} plot={this.state.plot}/>
       </div>
     );
   }
--- homework/react/bootflix/src/components/Movie.jsx
+++ homework/react/bootflix/src/components/Movie.jsx
@@ -2,17 +2,19 @@ import React from "react";
 
 // Update this Movie component with info from OMDB
 // BONUS: Use OMDB's Poster API to add a poster to each movie.
-const Movie = () => {
+const Movie = (props) => {
+ 
+
   return (
     <section id="movie-listing">
       <div className="movie">
-        <h3>Movie Title</h3>
+        <h3>Movie Title: {props.title}</h3>
         <p>
-          <strong>Released:</strong> 1984<br />
-          <strong>Directed By:</strong> Some director<br />
-          <em>Genre:</em>
+          <strong>Released: {props.year}</strong> <br />
+          <strong>Directed By:</strong> {props.director}<br />
+          <em>Genre: {props.genre}</em>
         </p>
-        <p>Plotline should go inside of here</p>
+        <p>{props.plot}</p>
       </div>
     </section>
   );
