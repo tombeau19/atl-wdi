fcbc8b552804ec15e475c11163bf0c3853a7fd33
tombeau19 <thomas.beauregard@gmail.com>
2017-10-24 19:59:12 -0400

    3. Integers Skills

--- homework/ruby/ruby-ninja.rb
+++ homework/ruby/ruby-ninja.rb
@@ -1,18 +1,28 @@
-# Challenge 1
+############### Challenge 1 ##############
 # a = true
 # b = false
 
 # puts a == b
 # puts a != b
 
-# Challenge 2
-a = NIL
+############### Challenge 2 ###############
+# a = NIL
 
-p a  
-puts a
+# p a  
+# puts a
 
-p "#{a} was here"
+# p "#{a} was here"
 
-puts a.class 
+# puts a.class 
+
+############### Challenge 2 ###############
+d = Fixnum
+puts d
+
+e = Float
+puts e
+
+d = e
+puts d
 
 
